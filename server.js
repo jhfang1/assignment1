b23b73d6d1cb4342ee2e7d1b071aa9840d1da9dc
jhfang1 <143076092+jhfang1@users.noreply.github.com>
2023-11-21 13:33:54 -1000

    working products_data.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const product_data = require(__dirname + "/public/drinks.json");
+const product_data = require(__dirname + "/drinks.json");
 const qs = require('querystring');
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,12 +10,14 @@ app.all('*', function (request, response, next) {
   next();
 });
 
-app.get("product_data.js", function (request, response, next) {
-  response.type('.js');
-  let products_str = `var products = ${JSON.stringify(product_data)};`;
-  response.send(products_str);
+//Server gives product data to page by defining products variable
+app.get("/product_data.js", function (req, res) {
+  res.type('.js');
+  res.send(`let drinks = ${JSON.stringify(product_data)}`)
 });
 
+//rootdir will be in public starting from index.html
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+//Start Server
+app.listen(8080, () => console.log(`listening on port 8080`)); 
