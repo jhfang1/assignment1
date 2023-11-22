92eb8bb185cb0ccac0d1527055669099f342a642
jhfang1 <143076092+jhfang1@users.noreply.github.com>
2023-11-21 17:17:39 -1000

    update to invoice

--- server.js
+++ server.js
@@ -16,8 +16,25 @@ app.get("/product_data.js", function (req, res) {
   res.send(`let drinks = ${JSON.stringify(product_data)}`)
 });
 
+//Validate Purcahse
+app.post('/process_purchase', function (request, response, next) {
+  console.log(request.body);
+  // validate quantities 
+  var q = request.body['quantity_textbox'];
+  if (typeof q != 'undefined') {
+      response.send(`<b>Thank you for purchasing ${q} things!</b>`);
+  }});
+
 //rootdir will be in public starting from index.html
 app.use(express.static(__dirname + '/public'));
 
 //Start Server
 app.listen(8080, () => console.log(`listening on port 8080`)); 
+
+function isNonNegInt(q, returnErrors = false) {
+  errors = []; //assume no errors at first
+  if (Number(q) !=q) errors.push('Please enter a Number!'); //Check if string is a number value
+  if (q < 0) errors.push('Amount cannot be negative'); //Check if non negative
+  if (parseInt(q) !=q) errors.push('Must be a whole number'); //Check that it is an integer
+  return returnErrors ? errors : errors.length == 0; //return errors
+}
